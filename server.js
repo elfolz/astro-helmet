0892903c22f20de459c8654b98aceebce9e16f8d
Luiz <lhvalentim@outlook.coom>
2022-05-28 15:50:35 -0300

    open camera

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http")
+const fs = require('fs')
+
+const requestListener = (req, res) => {
+	if (req.url.endsWith('.png')) {
+		let png = fs.readFileSync(`${__dirname}${req.url}`)
+		res.setHeader('Content-Type', 'image/png')
+		return res.end(png)
+	}
+	let html = fs.readFileSync(`${__dirname}/index.html`)
+	res.setHeader('Content-Type', 'text/html')
+	res.end(html)
+}
+
+const server = http.createServer(requestListener)
+
+server.listen(8080, 'localhost')
